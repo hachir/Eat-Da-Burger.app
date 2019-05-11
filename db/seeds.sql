c5c92b615bbba39bb46ebe64d185f7295d3fb69e
hachir <ha1991ha@yahoo.com>
2019-05-11 11:56:39 -0600

    my app

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO BURGERS (burger_name, devoured) VALUES ('Chees Burger', FALSE);
+INSERT INTO BURGERS (burger_name, devoured) VALUES ('Turkey Burger', FALSE);
+INSERT INTO BURGERS (burger_name, devoured) VALUES ('Veggie Burger', FALSE);
